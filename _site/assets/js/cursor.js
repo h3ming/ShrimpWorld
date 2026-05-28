d34ce9049893d2f7c00849c8e1d1aa359364f136
h3ming <62722344+h3ming@users.noreply.github.com>
2026-05-28 16:04:38 -0500

    oops forgot gitignore

--- _site/assets/js/cursor.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  new cursoreffects.bubbleCursor({
-    fillColor: "rgba(255,255,255,0.15)",
-    strokeColor: "rgba(200,240,255,0.6)"
-  });
-});
